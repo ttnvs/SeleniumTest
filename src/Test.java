dbabc5ec45c950459cf29758318a88fef7d2922a
tsemenyshyna <tetiana.semenyshyna@ellucian.com>
2020-07-24 20:47:58 +0100

    add test class

---
+++ src/Test.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class Test {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "resources\\chromedriver.exe");		
+		WebDriver driver = new ChromeDriver();
+		
+		//Please write your code here
+
+		
+	}
+}
