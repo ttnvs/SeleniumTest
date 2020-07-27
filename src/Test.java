a51e15d61758b39c953c9fa1f728a5d4434a05ea
tsemenyshyna <tetiana.semenyshyna@ellucian.com>
2020-07-27 12:25:23 +0100

    update chromedriver version

--- src/Test.java
+++ src/Test.java
@@ -5,11 +5,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Test {
 
 	public static void main(String[] args) throws InterruptedException {
-		System.setProperty("webdriver.chrome.driver", "resources\\chromedriver.exe");		
+		System.setProperty("webdriver.chrome.driver", "resources\\chromedriver_v84.exe");
 		WebDriver driver = new ChromeDriver();
 		
 		//Please write your code here
 
-		
+
 	}
 }
