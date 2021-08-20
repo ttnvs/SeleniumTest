a243f9e62a1ad1cd96505bedf68fad7e02d5ded8
tsemenyshyna <tetiana.semenyshyna@ellucian.com>
2021-08-20 22:15:27 +0100

    Update chromedriver version to v92

--- src/Test.java
+++ src/Test.java
@@ -1,15 +1,18 @@
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 
+
 public class Test {
 
-	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver", "resources\\chromedriver_v85.exe");
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "resources\\chromedriver_v92.exe");
 		WebDriver driver = new ChromeDriver();
 		
 		//Please write your code here
-
 
 	}
 }
