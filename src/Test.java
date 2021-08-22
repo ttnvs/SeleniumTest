9b60084c184900d933a6cff6e6447b9ce1da610a
tatiana <tatiana.vvs@gmail.com>
2021-08-22 11:24:29 +0100

    Update chromedriver to v92 for mac

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,3 @@
-import org.junit.Assert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -8,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Test {
 
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "resources\\chromedriver_v92.exe");
 		WebDriver driver = new ChromeDriver();
 		
